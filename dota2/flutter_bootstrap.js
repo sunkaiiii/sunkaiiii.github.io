11d3be284358d09cf43fcfcbb89858a8d2d6a74f
孙楷 (Kai Sun) <ksun0017@outlook.com>
2026-07-09 22:59:02 +0930

    update

--- dota2/flutter_bootstrap.js
+++ dota2/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3292428260" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "456936403" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
